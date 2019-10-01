7fc3ba03a4f9d7a100261d8b3bb5e7db607cfe2f
kpanic <anhptvolga@yandex.ru>
2019-10-01 10:57:09 +0700

    first commit

---
+++ src/main/java/vn/tiki/genwiremock/GenWiremockApplication.java
@@ -0,0 +1,13 @@
+package vn.tiki.genwiremock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenWiremockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenWiremockApplication.class, args);
+    }
+
+}
